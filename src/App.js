427b91daeee018660f08ea5c05861b046108e34f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-09 15:12:48 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './SdkGlobalSquare.css';
 import './SdkMiddleSquare.css';
 import './SdkAtomicSquare.css';
 
+const a19 = new Array(9).fill(0).map((n, i) => i + 1);
+
 const SdkAtomicSquare = props => {
-  const array = new Array(9).fill(0).map((n, i) => i + 1);
+
 
   return (
     <div className={'frame ' + (props.isHighlighted ? 'hightlight' : '')} onClick={console.log('click')} >
@@ -13,7 +15,7 @@ const SdkAtomicSquare = props => {
         {props.value || ' '}
       </div>
 
-      {array.map(n => <div className={'possible-value pos-' + n}>{n}</div>)}
+      {a19.map(n => <div className={'possible-value pos-' + n}>{n}</div>)}
     </div>
   );
 };
@@ -22,7 +24,7 @@ const SdkMiddleSquare = props => (
   <ul className="middle-square">
     {[0, 1, 2].map(i => (
       <li key={i}>
-        {[0, 1, 2].map(j => <SdkAtomicSquare key={j} row={props.row + i} col={props.col + j} {...props}/>)}
+        {[0, 1, 2].map(j => <SdkAtomicSquare key={j} row={props.row + i} col={props.col + j} {...props} />)}
       </li>
     ))}
   </ul>
@@ -32,15 +34,44 @@ const SdkGlobalSquare = props => (
   <ul className="global-square">
     {[0, 3, 6].map(i => (
       <li key={i}>
-        {[0, 3, 6].map(j => <SdkMiddleSquare key={j} row={i} col={j} {...props}/>)}
+        {[0, 3, 6].map(j => <SdkMiddleSquare key={j} row={i} col={j} {...props} />)}
       </li>
     ))}
   </ul>
 );
 
-const SdkCommand = props => (
-  <div>SdkCommand</div>
-);
+const SdkCommand = props => {
+  const className = 'digit '
+    + (props.mode === 'assistant' ? 'assistant' : '');
+
+  return (
+    <React.Fragment>
+      <div>
+        {a19.map(i => (
+        <button 
+        className={className + (i === props.value ? 'active' : '')} 
+        key={i} 
+        onClick={console.log('click')}>
+        { i }
+        </button>
+      ))}
+
+      </div>
+      <div>
+        <button class="real" onClick={console.log('click')}>REAL MODE</button>
+        <button onClick={console.log('click')}>NEW SUDOKU</button >
+      </div >
+
+      <button onClick={console.log('click')}>Log</button>
+      <select name="difficulty">
+        <option value="0">Easy</option>
+        <option value="1">Medium</option>
+        <option value="2">Hard</option>
+      </select >
+    </React.Fragment >
+
+  );
+};
 
 const SdkSudoku = props => (
   <React.Fragment>
@@ -69,7 +100,7 @@ class App extends Component {
     this.state = {};
   }
   render() {
-    return <Root {...this.state}/>;
+    return <Root {...this.state} />;
   }
 }
 
