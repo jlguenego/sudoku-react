9c08cd30dc12a0c6e9b5e0d3d4c08b4c94b8e9d1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-09 14:17:31 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const SdkGlobalSquare = () => (
+  <div>SdkGlobalSquare</div>
+);
+const SdkCommand = () => (
+  <div>SdkCommand</div>
+);
+
 const SdkSudoku = () => (
-  <div>SdkSudoku</div>
+  <React.Fragment>
+    <SdkGlobalSquare />
+
+    <SdkCommand />
+  </React.Fragment>
+
 );
 
 const Root = () => (
@@ -12,7 +24,7 @@ const Root = () => (
       <div class="container">
         <h1>Sudoku</h1>
         <div class="text-center">
-          <SdkSudoku/>
+          <SdkSudoku />
         </div>
       </div>
     </main>
